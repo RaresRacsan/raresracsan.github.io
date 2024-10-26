6df7d1d98af76afc6cb795341f08efadb239d919
916-Racsan-Rares <82086767+RaresRacsan@users.noreply.github.com>
2024-10-26 12:57:21 +0300

    added photos, links work

--- js/projects.js
+++ js/projects.js
@@ -1,5 +1,36 @@
 let slideIndex = 1;
-showSlides(slideIndex);
+
+document.addEventListener("DOMContentLoaded", function() {
+    showSlides(slideIndex);
+    setArrowColor();
+});
+
+function setArrowColor() {
+    const currentSlide = document.querySelector('.slide:not([style*="display: none"])');
+    if (currentSlide) {
+        const img = currentSlide.querySelector('img');
+        const prevArrow = document.querySelector('.prev');
+        const nextArrow = document.querySelector('.next');
+
+        const canvas = document.createElement('canvas');
+        const ctx = canvas.getContext('2d');
+        canvas.width = img.width;
+        canvas.height = img.height;
+
+        // Draw image and get pixel data
+        ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+        const pixelData = ctx.getImageData(0, 0, 1, 1).data;
+        const brightness = (0.299 * pixelData[0] + 0.587 * pixelData[1] + 0.114 * pixelData[2]) / 255;
+
+        if (brightness > 0.5) {
+            prevArrow.style.color = '#333';
+            nextArrow.style.color = '#333';
+        } else {
+            prevArrow.style.color = '#fff';
+            nextArrow.style.color = '#fff';
+        }
+    }
+}
 
 function plusSlides(n) {
     showSlides(slideIndex += n);
